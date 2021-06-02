ef427d374821ef9e33f4bd39a2675dda97d5715b
Justin Grathwohl <justin.grathwohl@gmail.com>
2021-06-02 11:37:08 -0700

    cleanup

--- Active Directory Audit.ps1
+++ Active Directory Audit.ps1
@@ -16,12 +16,12 @@ function folderCheck ($path)
         }
     else
         {
-            Write-Output "$path does not exisit." 
+            Write-Output "$path does not exisit."
             Write-Output "Creating $path now"
             New-Item -ItemType Directory -Path $path|Out-Null
         }
 }
- 
+
 #Function for Write-Progress on overall script progression
 function overallProgress ($status)
 {
@@ -31,15 +31,15 @@ function overallProgress ($status)
     $percentage = [math]::Round($progressPercent)
     Write-Progress -Activity "Active Directory Audit" -Status "Progress" -PercentComplete $percentage
 }
- 
+
 #Overall Progress Report 1
 $overallStatus = 0
 overallProgress -status $overallStatus
 #end overall progress
- 
+
 #set default path
 $defaultPath = "$env:USERPROFILE\desktop\AD Audit" #end set default path
- 
+
 #Begin verification $exportPath is not empty
 if ([string]::IsNullOrWhiteSpace($exportPath))
 {
@@ -48,7 +48,7 @@ if ([string]::IsNullOrWhiteSpace($exportPath))
     Write-Output "Data will be exported to $exportPath"
 } 
 else { Write-Output "Data will be exported to $exportPath"}#end $exportPath verification
- 
+
 #folder list array
 $folders = @("$exportPath",
 "$exportPath\AD Groups",
@@ -58,11 +58,11 @@ $folders = @("$exportPath",
 "$exportPath\Inactive Items",
 "$exportPath\Disabled Items",
 "$exportPath\DC Information") #end folder list array
- 
+
 #Overall Progress Report 2
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #default inacticty days
 $defaultInactive = 30
 #Begin verification $daysInactive is not empy
@@ -73,67 +73,67 @@ if ($daysInactive -eq 0)
  Write-Output "Set number of days inactive to $daysInactive"   
 }
 else {Write-Output "Account inactivty threshold is $daysInactive"}#end $daysInactive verification
- 
+
 #Overall Progress Report 3
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #Set $time equal to $daysInactive from the date script is run
 $time = (Get-Date).AddDays(-($daysInactive))#end setting $time
- 
+
 #Overall Progress Report 4
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #gather all Groups and setting group variables
 $adGroupList = Get-ADGroup -Filter * #end gather all AD Groups
- 
+
 #Overall Progress Report 5
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #gather all enabled users in AD
 $userList = Get-ADUser -Filter {enabled -eq $true} -Properties lastLogonTimestamp,enabled,Description,CanonicalName #end gather all enabled users in AD
- 
+
 #Overall Progress Report 6
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #gather all GPOs
 $gpos = Get-GPO -All #end gather all GPOs
- 
+
 #Overall Progress Report 7
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #get inactive users
 $inactiveUsers = Get-ADUser -Filter{LastLogonTimeStamp -le $time -and enabled -eq $true} -Properties lastLogonTimestamp,enabled,Description,CanonicalName #end gather inactive users
- 
+
 #Overall Progress Report 8
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #get inactive computers
 $inactiveComputers = Get-ADComputer -Filter {LastLogonDate -le $time} -Properties LastLogonDate,CanonicalName #end get inactive computers
- 
+
 #Overall Progress Report 9
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #get disabled users
 $disabledUsers = Get-ADUser -Filter {enabled -eq $false} -Properties lastLogonTimestamp,enabled,Description,CanonicalName #end get disabled users
- 
+
 #Overall Progress Report 10
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #get disabled computers
 $disabledComputers = Get-ADComputer -Filter {enabled -eq $false} -Properties LastLogonDate,CanonicalName #end get disabled computers
- 
+
 #Overall Progress Report 11
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #check for directories
 $folderCount = $folders.Count
 $foldersProccessed = 0 
@@ -146,18 +146,18 @@ foreach ($folder in $folders)
     folderCheck -path $folder
     $foldersProccessed += 1
 } #end check for directories
- 
+
 #Overall Progress Report 12
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #export group lists
 $adGroupList|Select-Object name,groupcategory,groupscope,samaccountname| Export-Csv -path "$exportPath\AD Groups\All Groups.csv" -NoTypeInformation #end export group lists
- 
+
 #Overall Progress Report 13
 $overallStatus += 1
 overallProgress -status $overallStatus #end Overall Progress Report
- 
+
 #gather all users in groups
 $groupProcessed = 0
 $groupCount = $adGroupList.count
